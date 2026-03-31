6c934f685538e185d2a2fc203417e3cf1a66b2c4
C0DECREATURE <54833458+C0DECREATURE@users.noreply.github.com>
2026-03-31 05:30:59 -0400

    updates

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-#### Redirect to Warnings Page
+# Warnings Page Redirect
 This is a little snippet for a basic Javascript and HTML system to redirect users to a warnings page.
-You can have as many different warnings pages as you want for your site.
+
+### How to Use
+1. Download the code.
+2. Put the warnings folder anywhere you want on your site.
+3. If you put the folder somewhere other than the root directory, edit the variable "warningsURL" inside the warnings-redirect.js file to point to the path for your warnings folder.
+4. On any page you want to show the warnings, include this at the top of the page head: `<script src="/warnings/warnings-redirect.js"></script>` Make sure the src link has the correct path to where you put your folder.
+
+
+### Multiple Warning Pages
+If you want more than one completely separate warning page, make multiple copies of the warnings folder with different names. Edit the warnings-redirect.js file in each of those folders to point to the folder it's in.
+
+When putting the script link in the page head, set the src link to the warnings folder you want to use for this page.
--- warnings/warnings-redirect.js
+++ warnings/warnings-redirect.js
@@ -1,17 +1,24 @@
 // self executing function to set up warnings redirect
 (function(){
-	// EDIT THIS TO THE FILE PATH FOR THE WARNINGS PAGE
+	// EDIT THIS TO THE FILE PATH FOR THE WARNINGS FOLDER
 	let warningsURL = '/warnings';
+	
+	// get a usable name for this warnings folder
+	// this is what will be used for the localStorage showWarnings setting
+	let warningsName = warningsURL.replaceAll('/','');
 	
 	let urlParams = new URL(window.location.href).searchParams;
 	let showWarnings = urlParams.get('showWarnings');
 	// redirect to warnings page if criteria are met
 	if (
-		localStorage.getItem("showWarnings") != 'false'				// if user hasn't asked not to be shown warnings again
-		&& (!showWarnings || showWarnings != 'false')					// AND this page wasn't specifically loaded without warnings
-		&& navigator.userAgent.toLowerCase() != 'screenjesus'	// AND user is not the Neocities screenshot bot
+		// if user hasn't asked not to be shown warnings again
+		localStorage.getItem(`showWarnings${warningsName}`) != 'false'
+		// AND this page wasn't specifically loaded without warnings
+		&& (!showWarnings || showWarnings != 'false')
+		// AND user is not the Neocities screenshot bot
+		&& navigator.userAgent.toLowerCase() != 'screenjesus'
 	) {
-		// pass on the current URL to send users back to after viewing the warnings
+		// pass on the current URL. this is where the user will be sent after viewing the warnings
 		let redirect = window.location.pathname + window.location.search;
 		// send the user to the warnings page
 		window.location.href = `${warningsURL}?redirect=${redirect}`;
